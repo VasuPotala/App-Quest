c43b1c77cc252b0898519def5a52ba661b8fb021
sasi-kanakinti <sasi-kanakinti@users.noreply.github.com>
2024-10-09 12:03:29 +0530

    Edit Adminligin.php

--- adminlogin.php
+++ adminlogin.php
@@ -20,21 +20,21 @@ if($input_username=='vasu@gmail.com' and $input_password=='1234')
     $sql = "SELECT * FROM DonarDetails";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
-        echo '<h2>Donar Details</h2>';
-        echo '<table border="1">';
+        echo '<h2 style="text-align:center;color:auqa;font-variant:small-caps;">Donar Details</h2>';
+        echo '<table border="1px" border-collapse: separate  style="text-align: center;  margin-left: auto; margin-right: auto;">';
         echo '<tr><th>Donarname</th><th>mobilenumber </th><th>Donar_location </th><th>foodtype</th><th>quantity</th></tr>';
 
         // Output data of each row
         while($row = $result->fetch_assoc()) {
             echo "<tr>";
-            echo "<td>" . $row["Donarname"] . "</td>";
+            echo "<td style='color:red;'>" . $row["Donarname"] . "</td>";
             echo "<td>" . $row["mobilenumber"] . "</td>";
             echo "<td>" . $row["Donar_location"] . "</td>";
             echo "<td>" . $row["foodtype"] . "</td>";
             echo "<td>" . $row["quantity"] . "</td>";
+            
             echo "</tr>";
         }
-
         echo '</table>';
     } else {
         echo "No records found in the DonarDetails table.";
